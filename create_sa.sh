da3b4e0deb829cde653a749fabc34bd67a48905c
brayaON <bortizf@unal.edu.co>
2023-07-19 09:36:11 -0500

    Add comments for each operation

--- create_sa.sh
+++ create_sa.sh
@@ -8,6 +8,7 @@ az group show \
     --resource-group $AZURE_RG_NAME \
     2> /dev/null 1>&2
 
+# Create Resource Group if it doesn't exist
 if [[ $? -ne 0 ]]; then
     printf "Creating %s resource group...\n" $AZURE_RG_NAME
     az group create \
@@ -22,6 +23,7 @@ az storage account show \
     --name $AZURE_SA_NAME \
     2> /dev/null 1>&2
 
+# Create Storage Account if it doesn't exist
 if [[ $? -ne 0 ]]; then
     printf "Creating %s storage account..." $AZURE_SA_NAME
 
@@ -31,11 +33,12 @@ if [[ $? -ne 0 ]]; then
 	--sku Standard_LRS \
 	--location $location \
 	--default-action Deny \
-	--allow-blob-public-access true
+	--allow-blob-public-access false
 else
     printf "Skipping storage creation. %s storage account already exists...\n" $AZURE_SA_NAME
 fi
 
+# Add Client IP to the Storage Account firewall rule
 printf "Adding client IP to the %s storage account firewall rules...\n" $AZURE_SA_NAME
 myIP=`dig +short myip.opendns.com @resolver2.opendns.com`
 az storage account network-rule add \
@@ -48,6 +51,7 @@ if [[ $? -ne 0 ]]; then
     printf "Could not add the client IP to the %s Storage Account firewall rules" $AZURE_SA_NAME
 fi
 
+# Save Storage Account connection string
 AZURE_SA_CONN_STR=`az storage account show-connection-string \
 		    --name $AZURE_SA_NAME \
 		    --resource-group $AZURE_RG_NAME \
@@ -57,7 +61,7 @@ if [[ $? -ne 0 ]]; then
     printf "Could export the %s Storage Account Connection String...\n" $AZURE_SA_NAME
 fi
 
-
+# Save Storage Account Share Key
 AZURE_SA_KEY=`az storage account keys list \
 	    --account-name $AZURE_SA_NAME \
 	    --resource-group $AZURE_RG_NAME \
